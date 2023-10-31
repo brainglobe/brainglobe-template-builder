8d9a38a782ee65a3feaaa1a87d6d64b7ef7b41dc
niksirbi <niko.sirbiladze@gmail.com>
2023-10-31 10:59:28 +0000

    initialised napari plugin

---
+++ brainglobe_template_builder/napari/__init__.py
@@ -0,0 +1,19 @@
+import setuptools_scm
+
+try:
+    release = setuptools_scm.get_version(root="../..", relative_to=__file__)
+    __version__ = release.split("+")[0]  # remove git hash
+except LookupError:
+    __version__ = "unknown"
+
+from brainglobe_template_builder.napari._reader import napari_get_reader
+from brainglobe_template_builder.napari._widget import (
+    ExampleQWidget,
+    example_magic_widget,
+)
+
+__all__ = (
+    "napari_get_reader",
+    "ExampleQWidget",
+    "example_magic_widget",
+)
