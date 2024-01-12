2d7e0366cb12eac606a424da1eb90e5d05bd8fe6
niksirbi <niko.sirbiladze@gmail.com>
2024-01-12 13:26:06 +0000

    refactor preproc.py into two separate files

---
+++ brainglobe_template_builder/preproc/__init__.py
@@ -0,0 +1,6 @@
+from brainglobe_template_builder.preproc.masking import create_mask
+from brainglobe_template_builder.preproc.alignment import (
+    get_midline_points,
+    get_alignment_transform,
+    apply_transform,
+)
