540ec49cbf551b3b992bfdb916165b9b1b582627
niksirbi <niko.sirbiladze@gmail.com>
2024-06-21 18:17:28 +0100

    fixes slurm script for batching across averages

--- examples/slurm_batch_across_average_types.sh
+++ examples/slurm_batch_across_average_types.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #SBATCH -J ave_type # job name
-#SBATCH -p cpu # partition
+#SBATCH -p gpu # partition
 #SBATCH -N 1   # number of nodes
-#SBATCH --mem 128G # memory pool for all cores
+#SBATCH --mem 64G # memory pool for all cores
 #SBATCH -n 20 # number of cores
-#SBATCH -t 5-00:00 # time (D-HH:MM)
+#SBATCH -t 3-00:00 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%N.%j.out # write STDOUT
 #SBATCH -e slurm.%x.%N.%j.err # write STDERR
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=n.sirmpilatze@ucl.ac.uk
+#SBATCH --mail-user=ucqfnsi@ucl.ac.uk
 #SBATCH --array=0-2
 
 # Load the required modules
 module load miniconda
 module load ants
 
-# Activate the conda environment (because we need parallel and qbatch)
-conda activate template-builder
+# Activate the conda environmen
+conda activate cobralab
 
 # Define the atlas-forge directory and species name
 ATLAS_DIR="/ceph/neuroinformatics/neuroinformatics/atlas-forge"
@@ -28,11 +28,17 @@ AVE_TYPES=(median trimmed_mean efficient_trimean)
 AVE_TYPE=${AVE_TYPES[$SLURM_ARRAY_TASK_ID]}
 
 # Original template created via mean averaging
-TEMP_NAME_PREFIX="template_asym_res-50um_n-8"
+TEMP_NAME_PREFIX="template_hemi_res-50um_n-18"
 # Get only the first 4 characters of the average type
-AVE_TYPE_SHORT=$(echo $AVE_TYPE | cut -c1-4)
+AVE_TYPE_SHORT=$(echo "$AVE_TYPE" | cut -c1-4)
 # Define the new template name
 TEMP_NAME="${TEMP_NAME_PREFIX}_average-${AVE_TYPE_SHORT}"
+
+# Create the working directory where the template will be built
+mkdir -p "${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME}"
+# Copy brain_paths.txt and mask_paths.txt from the original template
+cp "${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME_PREFIX}/brain_paths.txt" "${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME}"
+cp "${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME_PREFIX}/mask_paths.txt" "${ATLAS_DIR}/${SPECIES}/templates/${TEMP_NAME}"
 
 # Path to the bash script that builds the template
 BUILD_SCRIPT="${ATLAS_DIR}/${SPECIES}/scripts/BlackCap_build_template.sh"
