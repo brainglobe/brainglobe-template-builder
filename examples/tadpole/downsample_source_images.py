141432155f6b3a7f5cab3dc312a1b0d160983509
alessandrofelder <alessandrofelder@users.noreply.github.com>
2024-09-20 16:35:23 +0100

    make general convenience function for read-downsample-write

--- examples/tadpole/downsample_source_images.py
+++ examples/tadpole/downsample_source_images.py
@@ -1,36 +1,9 @@
 import argparse
 from pathlib import Path
 
-from brainglobe_utils.IO.image import read_z_stack, save_any
 from loguru import logger
 
-from brainglobe_template_builder.preproc.transform_utils import (
-    downsample_anisotropic_image_stack,
-)
-
-
-def downsample_tadpole(folder: Path) -> None:
-    logger.info(f"Downsampling {folder.name}...")
-    stack = read_z_stack(str(folder))
-
-    in_plane_resolution = 1
-    out_of_plane_resolution = 3
-    in_plane_factor = int(target_isotropic_resolution / in_plane_resolution)
-    axial_factor = int(target_isotropic_resolution / out_of_plane_resolution)
-    downsampled = downsample_anisotropic_image_stack(
-        stack, in_plane_factor=in_plane_factor, axial_factor=axial_factor
-    )
-
-    sample_id = str(folder).split("_")[1].lower()
-    channel = "blue" if str(folder).split("_")[2] == "488" else "orange"
-    sample_filename = (
-        f"sub-{sample_id}_res-{target_isotropic_resolution}"
-        f"um_channel-{channel}.tif"
-    )
-    save_any(downsampled, template_raw_data / sample_filename)
-
-    logger.info(f"{sample_filename} downsampled.")
-
+from brainglobe_template_builder.preproc.transform_utils import downsample
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Downsample source images")
@@ -61,16 +34,28 @@ if __name__ == "__main__":
     template_building_root = Path(args.template_building_root)
     target_isotropic_resolution = int(args.target_isotropic_resolution)
 
-    for subfolder_name in [
-        "rawdata",
-        "logs",
-        "derivatives",
-        "scripts",
-        "templates",
-    ]:
-        subfolder = template_building_root / subfolder_name
-        subfolder.mkdir(exist_ok=True, parents=True)
+    in_plane_resolution = 1
+    out_of_plane_resolution = 3
+    in_plane_factor = int(target_isotropic_resolution / in_plane_resolution)
+    axial_factor = int(target_isotropic_resolution / out_of_plane_resolution)
 
     template_raw_data = template_building_root / "rawdata"
-    for folder in source_data.iterdir():
-        downsample_tadpole(folder)
+    template_raw_data.mkdir(exist_ok=True, parents=True)
+
+    for sample_folder in source_data.iterdir():
+        logger.info(f"Downsampling {sample_folder.name}...")
+        sample_id = str(sample_folder).split("_")[1].lower()
+        channel = (
+            "blue" if str(sample_folder).split("_")[2] == "488" else "orange"
+        )
+        sample_filename = (
+            f"sub-{sample_id}_res-{target_isotropic_resolution}"
+            f"um_channel-{channel}.tif"
+        )
+        downsample(
+            sample_folder,
+            template_raw_data / sample_filename,
+            in_plane_factor,
+            axial_factor,
+        )
+        logger.info(f"{sample_filename} downsampled.")
