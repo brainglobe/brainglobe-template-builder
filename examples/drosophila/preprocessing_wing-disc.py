b846786bb771e9974c3c8c63f9f549d338709495
Skxsmy <kaixiang.kevin.shuai@outlook.com>
2025-03-21 18:26:15 +0000

    Repaired padding function parameter

--- examples/drosophila/preprocessing_wing-disc.py
+++ examples/drosophila/preprocessing_wing-disc.py
@@ -126,7 +126,8 @@ if __name__ == "__main__":
 
         # Apply median filter to the downsampled image
         mf_image = median_filter(down_sampled_image, size=3)
-        mf_image = np.pad(mf_image, ((15, 15), (0, 0), (0, 0)), mode='constant')
+        mf_image = np.pad(mf_image, ((15, 15), (0, 0), (0, 0)), mode='constant',
+                          constant_values=((0, 0), (0, 0), (0, 0)))
         mf_image = mf_image * (mf_image > 104)
         logger.debug("Applied median filter and padding to the downsampled image and set the threshold as 104")
 
