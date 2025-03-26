3bef072d1fe1d46e73a92860ddf2f160fad5fec8
Viktor Plattner <plattnerviktor@gmail.com>
2025-03-26 17:46:06 +0000

    adding mask to pad and flip

--- examples/rat/2_prep_lowres.py
+++ examples/rat/2_prep_lowres.py
@@ -19,20 +19,30 @@ subject_ids = [folder.name for folder in Path(project_folder_path).glob('*')
                if folder.is_dir() and folder.name.startswith('sub-')]
 
 
-# Initialize lists to store image paths, image data, and dimensions
+# Initialize lists to store image and mask paths and data, and dimensions
 image_arrays = []
 dimensions = []
+mask_arrays = []
 rat_image_paths = []
+rat_mask_paths = []
+
 
 for subject_id in subject_ids:
     rat_image_path = list(Path(project_folder_path).rglob(f"{subject_id}/{subject_id}_*_orig-asr_aligned.tif"))
     rat_image_paths.extend(rat_image_path)
+    rat_mask_path = list(Path(project_folder_path).rglob(f"{subject_id}/{subject_id}_*_orig-asr_label-brain_aligned.tif"))
+    rat_mask_paths.extend(rat_mask_path)
 
 # Read images and store their dimensions
-for img_path in rat_image_paths:
+for img_path, mask_path in zip(rat_image_paths, rat_mask_paths):
+    # images
     img_array = load_tiff(img_path)
     image_arrays.append(img_array)
     dimensions.append(img_array.shape)
+    # masks
+    mask_array = load_tiff(mask_path)
+    mask_arrays.append(mask_array)
+
 
 # Compute max dimensions along each axis
 max_z = max(dim[0] for dim in dimensions)
@@ -45,7 +55,7 @@ max_y += 20
 max_x += 20
 
 # Pad images to match the largest dimensions + 20 pixels all around and save them
-for img_path, img in zip(rat_image_paths, image_arrays):
+for img_path, img, mask_path, mask in zip(rat_image_paths, image_arrays, rat_mask_paths, mask_arrays):
     # Calculate how much padding is needed for each axis
     pad_z = max_z - img.shape[0]
     pad_y = max_y - img.shape[1]
@@ -64,21 +74,37 @@ for img_path, img in zip(rat_image_paths, image_arrays):
                         mode='constant',
                         constant_values=0)
 
-    # Generate new filename with '_padded'
+    # Apply padding to the mask
+    padded_mask = np.pad(mask,
+                        ((pad_z_start, pad_z_end),
+                         (pad_y_start, pad_y_end),
+                         (pad_x_start, pad_x_end)),
+                        mode='constant',
+                        constant_values=0)
+
+    # Generate new image and mask filename with '_padded'
     padded_filename = img_path.stem + "_padded.nii.gz"
     padded_filepath = img_path.parent / padded_filename
+    padded_mask_filename = mask_path.stem + "_padded.nii.gz"
+    padded_mask_filepath = mask_path.parent / padded_mask_filename
 
     save_as_asr_nii(padded_img, lowres_vox_sizes, padded_filepath)
+    save_as_asr_nii(padded_mask, lowres_vox_sizes, padded_mask_filepath)
 
     # Create the 'flipped' folder if it doesn't exist
     flipped_folder = img_path.parent / "flipped"
     flipped_folder.mkdir(parents=True, exist_ok=True)
 
-    # Flip the image along axis 2
+    # Flip the image and the mask along axis 2
     padded_flipped_img = np.flip(padded_img, axis=2)
+    padded_flipped_mask = np.flip(padded_mask, axis=2)
+
 
     # Construct new filename for the flipped image
     flipped_filename = img_path.stem + "_padded_flipped.nii.gz"
     flipped_filepath = flipped_folder / flipped_filename
+    flipped_mask_filename = mask_path.stem + "_padded_flipped.nii.gz"
+    flipped_mask_filepath = flipped_folder / flipped_mask_filename
 
     save_as_asr_nii(padded_flipped_img, lowres_vox_sizes, flipped_filepath)
+    save_as_asr_nii(padded_flipped_mask, lowres_vox_sizes,flipped_mask_filepath)
