f18cc0185aaa29d6e1c09f0cdcdead2f06accfad
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-11-12 15:02:28 +0000

    constrain range of numeric config values

--- brainglobe_template_builder/preproc/preproc_config.py
+++ brainglobe_template_builder/preproc/preproc_config.py
@@ -1,7 +1,8 @@
 from enum import Enum
 from pathlib import Path
+from typing import Annotated
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class ThresholdMethod(str, Enum):
@@ -11,18 +12,18 @@ class ThresholdMethod(str, Enum):
 
 
 class MaskConfig(BaseModel):
-    gaussian_sigma: float = 3
+    gaussian_sigma: Annotated[float, Field(ge=0)] = 3
     threshold_method: ThresholdMethod = ThresholdMethod.TRIANGLE
-    closing_size: int = 5
-    erode_size: int = 0
+    closing_size: Annotated[int, Field(ge=0)] = 5
+    erode_size: Annotated[int, Field(ge=0)] = 0
 
 
 class PreprocConfig(BaseModel):
     derivatives_dir: Path
-    resolution_z: float
-    resolution_y: float
-    resolution_x: float
+    resolution_z: Annotated[float, Field(gt=0)]
+    resolution_y: Annotated[float, Field(gt=0)]
+    resolution_x: Annotated[float, Field(gt=0)]
     mask: MaskConfig
-    pad_pixels: int = 5
+    pad_pixels: Annotated[int, Field(ge=0)] = 5
 
     model_config = ConfigDict(extra="forbid")
