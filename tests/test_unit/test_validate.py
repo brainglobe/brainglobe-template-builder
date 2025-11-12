0233da3847dd1b049874c75633f00cdd3f0e9f48
Stella <30465823+stellaprins@users.noreply.github.com>
2025-11-12 19:58:33 +0000

    add test_validate_required_columns

--- tests/test_unit/test_validate.py
+++ tests/test_unit/test_validate.py
@@ -5,6 +5,7 @@ import pytest
 
 from brainglobe_template_builder.validate import (
     validate_file_extension,
+    validate_required_columns,
 )
 
 
@@ -14,13 +15,13 @@ def valid_df():
     data = {
         "species": "Zebra finch",
         "sex": ["F", "M", "F", "M", "F"],
-        "subject_id": [f"ZF{i+1}" for i in range(5)],
+        "subject_id": [f"ZF{i + 1}" for i in range(5)],
         "resolution_z": 50,
         "resolution_y": 50,
         "resolution_x": 50,
         "channel": "green",
         "origin": ["PSL", "PSL", "LAS", "SAL", "LPI"],
-        "source_filepath": [f"/path/to/atlas-{i+1}" for i in range(5)],
+        "source_filepath": [f"/path/to/atlas-{i + 1}" for i in range(5)],
     }
     return pd.DataFrame(data)
 
@@ -54,3 +55,38 @@ def test_validate_file_extension(filepath, extension, is_valid):
         with pytest.raises(ValueError) as excinfo:
             validate_file_extension(filepath, extension)
         assert f"File must have {extension} extension" in str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    ["columns", "required", "error_message"],
+    [
+        pytest.param(
+            ["col1", "col2"], ["col1"], None, id="valid (extra col present)"
+        ),
+        pytest.param(
+            ["col1"],
+            ["col1", "col2"],
+            "Column with name 'col2' required but missing from source CSV.",
+            id="invalid (col2 missing)",
+        ),
+        pytest.param(
+            [],
+            ["col1", "col2"],
+            "Columns with names 'col1', 'col2' required but missing from"
+            " source CSV.",
+            id="invalid (multiple cols missing)",
+        ),
+        pytest.param(
+            ["col1", "col2"],
+            ["col1", "col2"],
+            None,
+            id="valid (all required present)",
+        ),
+    ],
+)
+def test_validate_required_columns(columns, required, error_message):
+    if error_message:
+        with pytest.raises(ValueError, match=error_message):
+            validate_required_columns(columns, required)
+    else:
+        validate_required_columns(columns, required)
