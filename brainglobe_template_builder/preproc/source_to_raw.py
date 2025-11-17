54cbb075ed02ea98f1ddc12dd4110192babb6c6d
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-11-17 10:59:17 +0000

    expand docstrings and handle output_vox_size is none

--- brainglobe_template_builder/preproc/source_to_raw.py
+++ brainglobe_template_builder/preproc/source_to_raw.py
@@ -8,17 +8,17 @@ from brainglobe_utils.IO.image.save import save_as_asr_nii
 from brainglobe_template_builder.plots import plot_orthographic
 
 
-def _process_subject(
-    subject_row: pd.Series,
-    output_dir: Path,
-    output_vox_size: float | None = None,
+def _get_subject_path(
+    output_dir: Path, subject_id: str, output_vox_sizes: list[float]
 ) -> Path:
+    """Get path to standardised nifti file for subject_id, and create any
+    required parent directories."""
 
-    image = load_any(subject_row.source_filepath)
-
-    subject_id = subject_row.subject_id
+    # round output vox sizes to nearest int (we don't want decimal
+    # points in the filename)
+    rounded_sizes = [round(vox_size) for vox_size in output_vox_sizes]
     resolution_string = (
-        f"res-{output_vox_size}x{output_vox_size}x{output_vox_size}um"
+        f"res-{rounded_sizes[0]}x{rounded_sizes[1]}x{rounded_sizes[2]}um"
     )
     dest_path = (
         output_dir
@@ -26,6 +26,42 @@ def _process_subject(
         / f"sub-{subject_id}_{resolution_string}_origin-asr.nii.gz"
     )
     dest_path.parent.mkdir(parents=True, exist_ok=True)
+
+    return dest_path
+
+
+def _process_subject(
+    subject_row: pd.Series,
+    output_dir: Path,
+    output_vox_size: float | None = None,
+) -> Path:
+    """Standardise source image of an individual subject.
+
+    A directory is created inside 'output_dir' for the subject containing:
+    - the standardised nifti image file
+    - a QC plot to verify the ASR orientation
+
+    Parameters
+    ----------
+    subject_row : pd.Series
+        Subject row from input csv file (in standardised format
+        defined in the atlas-forge documentation).
+    output_dir : Path
+        Output directory to write to.
+    output_vox_size : float | None, optional
+        Output voxel size in micrometre. Images will be downsampled to
+        an isotropic resolution of
+        output_vox_size x output_vox_size x output_vox_size. If not
+        provided, input images will not be downsampled.
+
+    Returns
+    -------
+    Path
+        Path to standardised nifti file.
+    """
+
+    image = load_any(subject_row.source_filepath)
+    subject_id = subject_row.subject_id
 
     # downsample to target isotropic resolution
     if output_vox_size is not None:
@@ -35,15 +71,23 @@ def _process_subject(
     space = AnatomicalSpace(subject_row.origin)
     image = space.map_stack_to("asr", image)
 
+    # Get path to output image file, incorporating output
+    # resolution into filename
+    if output_vox_size is None:
+        output_vox_sizes = [
+            subject_row.resolution_x,
+            subject_row.resolution_y,
+            subject_row.resolution_z,
+        ]
+    else:
+        output_vox_sizes = [output_vox_size, output_vox_size, output_vox_size]
+    dest_path = _get_subject_path(output_dir, subject_id, output_vox_sizes)
+
     # save QC plot of re-oriented image
     plot_path = dest_path.parent / f"sub-{subject_id}-QC-standardised.png"
     plot_orthographic(image, anat_space="ASR", save_path=plot_path)
 
-    vox_sizes_mm = [
-        subject_row.resolution_x * 0.001,
-        subject_row.resolution_y * 0.001,
-        subject_row.resolution_z * 0.001,
-    ]
+    vox_sizes_mm = [vox_size * 0.001 for vox_size in output_vox_sizes]
     save_as_asr_nii(image, vox_sizes=vox_sizes_mm, dest_path=dest_path)
 
     return dest_path
@@ -52,6 +96,28 @@ def _process_subject(
 def source_to_raw(
     input_csv: Path, output_dir: Path, output_vox_size: float | None = None
 ) -> None:
+    """Standardise source images to the same resolution and orientation (ASR).
+
+    A directory is created inside 'output_dir' for each subject id containing:
+    - the standardised nifti image file
+    - a QC plot to verify the ASR orientation
+
+    At the top level of 'output_dir', a csv file is created summarising the
+    properties / locations of the standardised image files.
+
+    Parameters
+    ----------
+    input_csv : Path
+        Input csv with one row per subject id. Should be in the standard
+        format defined in the atlas-forge documentation.
+    output_dir : Path
+        Output directory to write to.
+    output_vox_size : float | None, optional
+        Output voxel size in micrometre. Images will be downsampled to
+        an isotropic resolution of
+        output_vox_size x output_vox_size x output_vox_size. If not
+        provided, input images will not be downsampled.
+    """
 
     input_df = pd.read_csv(input_csv)
     # TODO - Validate input csv
@@ -67,8 +133,11 @@ def source_to_raw(
     # Make output csv for processed images
     output_df = input_df.copy()
     output_df.origin = "ASR"
-    output_df.resolution_z = output_vox_size
-    output_df.resolution_y = output_vox_size
-    output_df.resolution_x = output_vox_size
     output_df.source_filepath = processed_paths
+
+    if output_vox_size is not None:
+        output_df.resolution_z = output_vox_size
+        output_df.resolution_y = output_vox_size
+        output_df.resolution_x = output_vox_size
+
     output_df.to_csv(output_dir / "raw_images.csv", index=False)
