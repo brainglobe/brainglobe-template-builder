7c3a5933d6b749ca8c1eb3925f42dee34a637c4a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-12-01 13:08:34 +0000

    Add source_to_raw function (#98)
    * rough outline of source_to_raw module
    
    * add QC plot and re-orientation to ASR
    
    * create output csv with processed paths
    
    * expand docstrings and handle output_vox_size is none
    
    * enforce isotropic resolution for all images
    
    * create a raw dir inside output_dir
    
    * validate source csv
    
    * allow processing mask images from the source dir
    
    * update source_to_raw docstrings
    
    * rough outline of a source_to_raw test
    
    * add test for source_to_raw with masks
    
    * add test for use column with source_to_raw
    
    * add source_to_raw test for anisotropic data
    
    * refactor source_to_raw fixtures and tests
    
    * add test for re-orientation to ASR with no downsampling
    
    * add test for downsampling via source_to_raw
    
    * depend on latest brainglobe-utils for new read_with_dask features
    
    * add downsampling step to source_to_raw
    
    * expand docstrings for source_to_raw
    
    * fix downsampling test for source_to_raw
    
    * split source_to_raw filepath test into two
    
    * update test_source_to_raw docstrings
    
    * fix typo and expand _get_subject_path docstring
    
    * update axis order passed to _process_subject and add test

---
+++ brainglobe_template_builder/preproc/source_to_raw.py
@@ -0,0 +1,251 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from brainglobe_space import AnatomicalSpace
+from brainglobe_utils.IO.image.load import load_any, read_with_dask
+from brainglobe_utils.IO.image.save import save_as_asr_nii
+
+from brainglobe_template_builder.plots import plot_orthographic
+from brainglobe_template_builder.preproc.transform_utils import (
+    downsample_anisotropic_stack_to_isotropic,
+)
+from brainglobe_template_builder.validate import validate_input_csv
+
+
+def _get_subject_path(
+    raw_dir: Path, subject_id: str, output_vox_size: float, mask: bool = False
+) -> Path:
+    """Get path to standardised nifti file for subject_id, and create any
+    required parent directories.
+
+    For example, for a subject with ID 002 at voxel size 2.1 micrometre,
+    return "{raw_dir}/sub-002/sub-002_res-2x2x2um{_mask}_origin-asr.nii.gz".
+    """
+
+    # round output vox sizes to nearest int (we don't want decimal
+    # points in the filename)
+    rounded_size = round(output_vox_size)
+    resolution_string = f"res-{rounded_size}x{rounded_size}x{rounded_size}um"
+
+    subject_dir = raw_dir / f"sub-{subject_id}"
+    file_name = f"sub-{subject_id}_{resolution_string}"
+    if mask:
+        dest_path = subject_dir / f"{file_name}_mask_origin-asr.nii.gz"
+    else:
+        dest_path = subject_dir / f"{file_name}_origin-asr.nii.gz"
+
+    dest_path.parent.mkdir(parents=True, exist_ok=True)
+    return dest_path
+
+
+def _process_image(
+    image_path: Path,
+    output_path: Path,
+    origin: str,
+    input_vox_sizes: list[float],
+    output_vox_size: float,
+    mask: bool = False,
+) -> None:
+    """
+    Standardise a single source image.
+
+    Converts to ASR orientation, and downsamples to an isotropic
+    resolution of [output_vox_size, output_vox_size, output_vox_size].
+    Also saves QC plots for this image, to verify ASR orientation.
+
+    Parameters
+    ----------
+    image_path : Path
+        Path to image to standardise.
+    output_path : Path
+        Path to write processed image to.
+    origin : str
+        3-letter anatomical orientation code (e.g., PSL, LSP, RAS)
+    input_vox_sizes : list[float]
+        Input voxel sizes in microns - matching anatomical axis order.
+    output_vox_size : float
+        Output voxel size in microns.
+    mask : bool, optional
+        Whether the input image is a mask, by default False
+    """
+
+    if (np.array(input_vox_sizes) == output_vox_size).all():
+        image = load_any(image_path)
+    else:
+        # downsample to target resolution via dask to handle
+        # out-of-memory images
+        image_dask = read_with_dask(image_path)
+        image = downsample_anisotropic_stack_to_isotropic(
+            image_dask, input_vox_sizes, output_vox_size, mask=mask
+        )
+
+    # re-orient to ASR
+    space = AnatomicalSpace(origin)
+    image = space.map_stack_to("asr", image)
+
+    # save QC plot of re-oriented image
+    file_name = str(output_path.stem).split(".")[0]
+    plot_path = output_path.parent / f"{file_name}-QC.png"
+
+    if mask:
+        plot_orthographic(
+            image,
+            anat_space="ASR",
+            save_path=plot_path,
+            vmin=image.min(),
+            vmax=image.max(),
+        )
+    else:
+        plot_orthographic(image, anat_space="ASR", save_path=plot_path)
+
+    output_vox_sizes = [output_vox_size, output_vox_size, output_vox_size]
+    vox_sizes_mm = [vox_size * 0.001 for vox_size in output_vox_sizes]
+    save_as_asr_nii(image, vox_sizes=vox_sizes_mm, dest_path=output_path)
+
+
+def _process_subject(
+    subject_row: pd.Series,
+    raw_dir: Path,
+    output_vox_size: float | None = None,
+) -> tuple[Path, Path | None]:
+    """Standardise source images of an individual subject.
+
+    A directory is created inside 'raw_dir' for the subject containing:
+    - the standardised nifti image file
+    - the standardised nifti mask file (if a mask_filepath was provided)
+    - QC plots to verify the ASR orientation
+
+    Parameters
+    ----------
+    subject_row : pd.Series
+        Subject row from input csv file (in standardised format
+        defined in the atlas-forge documentation).
+    raw_dir : Path
+        Raw directory to write subject files to.
+    output_vox_size : float | None, optional
+        Output voxel size in micrometre. Images will be downsampled to
+        an isotropic resolution of
+        output_vox_size x output_vox_size x output_vox_size. If not
+        provided, input images will be left as-is (they must have
+        isotropic resolution!).
+
+    Returns
+    -------
+    tuple[Path, Path | None]
+        Returns: (
+            Path to standardised nifti image file,
+            Path to standardised nifti mask file - if provided, otherwise None
+        )
+    """
+
+    subject_id = subject_row.subject_id
+    input_vox_sizes = [
+        subject_row.resolution_z,
+        subject_row.resolution_y,
+        subject_row.resolution_x,
+    ]
+
+    # Enforce input images must have isotropic voxel size, if no
+    # downsampling
+    if output_vox_size is None:
+        if len(set(input_vox_sizes)) != 1:
+            raise ValueError(
+                f"Subject id: {subject_id} has anisotropic voxel size: "
+                f"{input_vox_sizes}. Pass an output_vox_size to re-sample it."
+            )
+        output_vox_size = input_vox_sizes[0]
+
+    # Process input image
+    image_output_path = _get_subject_path(
+        raw_dir, subject_id, output_vox_size, mask=False
+    )
+    _process_image(
+        Path(subject_row.source_filepath),
+        image_output_path,
+        subject_row.origin,
+        input_vox_sizes,
+        output_vox_size,
+        mask=False,
+    )
+
+    # Process input mask (if provided)
+    if ("mask_filepath" in subject_row) and pd.notna(
+        subject_row.mask_filepath
+    ):
+        mask_output_path = _get_subject_path(
+            raw_dir, subject_id, output_vox_size, mask=True
+        )
+        _process_image(
+            Path(subject_row.mask_filepath),
+            mask_output_path,
+            subject_row.origin,
+            input_vox_sizes,
+            output_vox_size,
+            mask=True,
+        )
+    else:
+        mask_output_path = None
+
+    return image_output_path, mask_output_path
+
+
+def source_to_raw(
+    source_csv: Path, output_dir: Path, output_vox_size: float | None = None
+) -> None:
+    """Standardise source images to the same resolution and orientation (ASR).
+
+    A 'raw' directory is created inside 'output_dir', containing a folder for
+    each subject id with:
+    - the standardised nifti image file
+    - the standardised nifti mask file (if a mask_filepath was provided)
+    - QC plots to verify the ASR orientation
+
+    At the top level of the 'raw' dir, a csv file is created summarising the
+    properties / locations of the standardised image files.
+
+    Parameters
+    ----------
+    source_csv : Path
+        Source csv with one row per subject id. Should be in the standard
+        format defined in the atlas-forge documentation.
+    output_dir : Path
+        Output directory to write to.
+    output_vox_size : float | None, optional
+        Output voxel size in micrometre. Images will be downsampled to
+        an isotropic resolution of
+        output_vox_size x output_vox_size x output_vox_size. If not
+        provided, input images will be left as-is (they must have
+        isotropic resolution!).
+    """
+
+    validate_input_csv(source_csv)
+    source_df = pd.read_csv(source_csv)
+
+    raw_dir = output_dir / "raw"
+    raw_dir.mkdir(parents=True, exist_ok=True)
+
+    if "use" in source_df:
+        source_df = source_df[source_df.use]
+
+    processed_image_paths = []
+    processed_mask_paths = []
+    for _, row in source_df.iterrows():
+        image_path, mask_path = _process_subject(row, raw_dir, output_vox_size)
+        processed_image_paths.append(image_path)
+        processed_mask_paths.append(mask_path)
+
+    # Make output csv for processed images
+    output_df = source_df.copy()
+    output_df.origin = "ASR"
+    output_df.source_filepath = processed_image_paths
+
+    if "mask_filepath" in output_df:
+        output_df.mask_filepath = processed_mask_paths
+
+    if output_vox_size is not None:
+        output_df.resolution_z = output_vox_size
+        output_df.resolution_y = output_vox_size
+        output_df.resolution_x = output_vox_size
+
+    output_df.to_csv(raw_dir / "raw_images.csv", index=False)
---
+++ tests/test_unit/test_source_to_raw.py
@@ -0,0 +1,419 @@
+from pathlib import Path
+from typing import Any
+
+import numpy as np
+import pandas as pd
+import pytest
+from brainglobe_space import AnatomicalSpace
+from brainglobe_utils.IO.image.load import load_nii
+from brainglobe_utils.IO.image.save import save_any
+from numpy.typing import NDArray
+
+from brainglobe_template_builder.preproc.source_to_raw import source_to_raw
+
+
+@pytest.fixture()
+def stack() -> NDArray[np.float64]:
+    """Create 50x50x50 stack with a small off-centre object (value 0.5).
+
+    The object is off-centre, so we can identify the effects of
+    re-orientation to ASR.
+    """
+    stack = np.zeros((50, 50, 50))
+    stack[10:30, 10:30, 10:30] = 0.5
+    return stack
+
+
+@pytest.fixture()
+def mask() -> NDArray[np.float64]:
+    """Create 50x50x50 binary mask with off-centre object (value = 1).
+
+    The object is off-centre, so we can identify the effects of
+    re-orientation to ASR.
+    """
+    mask = np.zeros((50, 50, 50))
+    mask[5:35, 5:35, 5:35] = 1
+    return mask
+
+
+@pytest.fixture()
+def source_dir(tmp_path) -> Path:
+    """Create a temporary 'source' directory."""
+    source_dir = tmp_path / "source"
+    source_dir.mkdir(parents=True, exist_ok=True)
+
+    return source_dir
+
+
+def create_test_images(
+    path: Path, test_data: list[dict[str, Any]]
+) -> list[dict[str, Any]]:
+    """Save test images/masks and add their paths to the
+    returned test data dicts."""
+
+    for data in test_data:
+        subject_dir = path / data["subject_id"]
+        subject_dir.mkdir()
+
+        image_path = subject_dir / f"{data['subject_id']}.tiff"
+        save_any(data["image"], image_path)
+        data["source_filepath"] = image_path
+
+        if data["mask"] is not None:
+            mask_path = subject_dir / f"{data['subject_id']}_mask.tiff"
+            save_any(data["mask"], mask_path)
+            data["mask_filepath"] = mask_path
+
+    return test_data
+
+
+def create_test_csv(path: Path, test_data: list[dict[str, Any]]) -> Path:
+    """Creates "source_data" CSV file and returns its path."""
+
+    # we don't include the image / mask in the csv file,
+    # just metadata about them
+    df_data = test_data.copy()
+    for data in df_data:
+        data.pop("image")
+        data.pop("mask")
+
+    input_csv = pd.DataFrame(data=df_data)
+    csv_path = path / "source_data.csv"
+    input_csv.to_csv(csv_path, index=False)
+    return csv_path
+
+
+def write_test_data(source_dir: Path, test_data: list[dict[str, Any]]) -> Path:
+    """Write test data to source_dir, and return the path to
+    the summary csv."""
+    test_data = create_test_images(source_dir, test_data)
+    csv_path = create_test_csv(source_dir, test_data)
+    return csv_path
+
+
+@pytest.fixture()
+def test_data(stack: NDArray[np.float64]) -> list[dict[str, Any]]:
+    """Creates test data for two images with LSA and PSL orientation."""
+    return [
+        {
+            "image": stack,
+            "mask": None,
+            "subject_id": "a",
+            "resolution_z": 25,
+            "resolution_y": 25,
+            "resolution_x": 25,
+            "origin": "PSL",
+        },
+        {
+            "image": stack,
+            "mask": None,
+            "subject_id": "b",
+            "resolution_z": 10,
+            "resolution_y": 10,
+            "resolution_x": 10,
+            "origin": "LSA",
+        },
+    ]
+
+
+@pytest.fixture()
+def source_csv_no_masks(
+    source_dir: Path, test_data: list[dict[str, Any]]
+) -> Path:
+    """Create test data for two subjects - neither of which
+    have masks."""
+
+    return write_test_data(source_dir, test_data)
+
+
+@pytest.fixture()
+def source_csv_with_masks(
+    source_dir: Path,
+    test_data: list[dict[str, Any]],
+    mask: NDArray[np.float64],
+) -> Path:
+    """Create test data for two subjects, one with a mask and
+    one without."""
+
+    test_data[0]["mask"] = mask
+    test_data[1]["mask"] = None
+
+    return write_test_data(source_dir, test_data)
+
+
+@pytest.fixture()
+def source_csv_single_image_with_mask(
+    source_dir: Path,
+    test_data: list[dict[str, Any]],
+    mask: NDArray[np.float64],
+) -> Path:
+    """Create test data for a single subject with a
+    corresponding mask."""
+
+    single_image = test_data[1]
+    single_image["mask"] = mask
+
+    return write_test_data(source_dir, [single_image])
+
+
+@pytest.fixture()
+def source_csv_with_use(
+    source_dir: Path, test_data: list[dict[str, Any]]
+) -> Path:
+    """Create test data for two subjects - one with use=False,
+    and the other with use=True."""
+
+    test_data[0]["use"] = False
+    test_data[1]["use"] = True
+
+    return write_test_data(source_dir, test_data)
+
+
+@pytest.fixture()
+def source_csv_anisotropic_with_mask(
+    source_dir: Path, stack: NDArray[np.float64], mask: NDArray[np.float64]
+) -> Path:
+    """Create test data for a single subject with anisotropic
+    resolution and a corresponding mask."""
+
+    return write_test_data(
+        source_dir,
+        [
+            {
+                "image": stack,
+                "mask": mask,
+                "subject_id": "b",
+                "resolution_z": 10,
+                "resolution_y": 4,
+                "resolution_x": 2,
+                "origin": "ASR",
+            }
+        ],
+    )
+
+
+@pytest.mark.parametrize(
+    "source_csv,expected_paths",
+    [
+        pytest.param(
+            "source_csv_no_masks",
+            [
+                "raw/sub-a",
+                "raw/sub-b",
+                "raw/raw_images.csv",
+                # subject a images + QC plots
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr-QC.png",
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr-QC.pdf",
+                # subject b images + QC plots
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr-QC.png",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr-QC.pdf",
+            ],
+            id="no mask column",
+        ),
+        pytest.param(
+            "source_csv_with_masks",
+            [
+                "raw/sub-a",
+                "raw/sub-b",
+                "raw/raw_images.csv",
+                # subject a images + masks + QC plots for both
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr-QC.png",
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr-QC.pdf",
+                "raw/sub-a/sub-a_res-50x50x50um_mask_origin-asr.nii.gz",
+                "raw/sub-a/sub-a_res-50x50x50um_mask_origin-asr-QC.png",
+                "raw/sub-a/sub-a_res-50x50x50um_mask_origin-asr-QC.pdf",
+                # subject b images + QC plots (no mask provided)
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr-QC.png",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr-QC.pdf",
+            ],
+            id="with mask column",
+        ),
+    ],
+)
+def test_source_to_raw_filepaths(request, source_csv, expected_paths):
+    """Test source to raw creates all the correct files, in the right
+    directory structure."""
+
+    source_csv_path = request.getfixturevalue(source_csv)
+    output_dir = source_csv_path.parents[1]
+    output_vox_size = 50
+    source_to_raw(source_csv_path, output_dir, output_vox_size)
+
+    # Check correct files / directory structure created
+    raw_dir = output_dir / "raw"
+    assert raw_dir.exists()
+
+    created_paths = list(raw_dir.glob("**/*"))
+    expected_paths = [output_dir / file for file in expected_paths]
+
+    assert sorted(created_paths) == sorted(expected_paths)
+
+
+@pytest.mark.parametrize(
+    "source_csv,expected_image_paths,expected_mask_paths",
+    [
+        pytest.param(
+            "source_csv_no_masks",
+            [
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr.nii.gz",
+            ],
+            [],
+            id="no mask column",
+        ),
+        pytest.param(
+            "source_csv_with_masks",
+            [
+                "raw/sub-a/sub-a_res-50x50x50um_origin-asr.nii.gz",
+                "raw/sub-b/sub-b_res-50x50x50um_origin-asr.nii.gz",
+            ],
+            ["raw/sub-a/sub-a_res-50x50x50um_mask_origin-asr.nii.gz", np.nan],
+            id="with mask column",
+        ),
+    ],
+)
+def test_source_to_raw_output_csv(
+    request, source_csv, expected_image_paths, expected_mask_paths
+):
+    """Test source to raw creates an output csv with the correct metadata."""
+
+    source_csv_path = request.getfixturevalue(source_csv)
+    output_dir = source_csv_path.parents[1]
+    output_vox_size = 50
+    source_to_raw(source_csv_path, output_dir, output_vox_size)
+
+    # Check output csv contains correct paths
+    output_csv_path = output_dir / "raw" / "raw_images.csv"
+    output_csv = pd.read_csv(output_csv_path)
+
+    expected_image_paths = [
+        str(output_dir / path) for path in expected_image_paths
+    ]
+    expected_mask_paths = [
+        str(output_dir / path) if not pd.isna(path) else path
+        for path in expected_mask_paths
+    ]
+
+    expected_output_csv = pd.DataFrame(
+        data={
+            "subject_id": ["a", "b"],
+            "resolution_z": output_vox_size,
+            "resolution_y": output_vox_size,
+            "resolution_x": output_vox_size,
+            "origin": "ASR",
+            "source_filepath": expected_image_paths,
+        }
+    )
+    if len(expected_mask_paths) > 0:
+        expected_output_csv["mask_filepath"] = expected_mask_paths
+
+    pd.testing.assert_frame_equal(output_csv, expected_output_csv)
+
+
+def test_source_to_raw_with_use(source_csv_with_use):
+    """Test source_to_raw excludes subjects with use=False."""
+
+    output_dir = source_csv_with_use.parents[1]
+    source_to_raw(source_csv_with_use, output_dir)
+
+    raw_dir = output_dir / "raw"
+    assert raw_dir.exists()
+    assert not (raw_dir / "sub-a").exists()  # subject a has use=False
+    assert (raw_dir / "sub-b").exists()  # subject b has use=True
+
+    output_csv_path = output_dir / "raw" / "raw_images.csv"
+    assert output_csv_path.exists()
+
+    output_csv = pd.read_csv(output_csv_path)
+    assert len(output_csv) == 1
+    assert output_csv.subject_id.iloc[0] == "b"
+
+
+def test_source_to_raw_anisotropic(source_csv_anisotropic_with_mask):
+    """Test source_to_raw errors when anisotropic data is provided with no
+    output_vox_size.
+    """
+
+    with pytest.raises(
+        ValueError,
+        match=r"Subject id: b has anisotropic voxel size: \[10, 4, 2\]",
+    ):
+        source_to_raw(
+            source_csv_anisotropic_with_mask,
+            source_csv_anisotropic_with_mask.parents[1],
+        )
+
+
+def test_source_to_raw_reorientation(
+    source_csv_single_image_with_mask, stack, mask
+):
+    """Test source_to_raw re-orients images and masks to ASR."""
+
+    output_dir = source_csv_single_image_with_mask.parents[1]
+    source_to_raw(source_csv_single_image_with_mask, output_dir)
+
+    subject_dir = output_dir / "raw" / "sub-b"
+    image_path = subject_dir / "sub-b_res-10x10x10um_origin-asr.nii.gz"
+    mask_path = subject_dir / "sub-b_res-10x10x10um_mask_origin-asr.nii.gz"
+
+    for output_path, source_image in zip(
+        [image_path, mask_path], [stack, mask]
+    ):
+        # No downsampling was specified, so input size / res should
+        # match output
+        image = load_nii(output_path, as_array=False)
+        assert image.header.get_zooms() == (
+            0.01,
+            0.01,
+            0.01,
+        )  # saved to nii as mm
+        assert image.shape == (50, 50, 50)
+
+        # Output should match re-orientation of source image to ASR
+        expected_image = AnatomicalSpace("LSA").map_stack_to(
+            "ASR", source_image
+        )
+        np.testing.assert_equal(image.get_fdata(), expected_image)
+
+
+@pytest.mark.parametrize(
+    "source_csv,expected_output_size",
+    [
+        pytest.param(
+            "source_csv_single_image_with_mask",
+            (25, 25, 25),
+            id="isotropic input - scale by 0.5, 0.5, 0.5",
+        ),
+        pytest.param(
+            "source_csv_anisotropic_with_mask",
+            (25, 10, 5),
+            id="anisotropic input - scale by 0.5, 0.2, 0.1",
+        ),
+    ],
+)
+def test_source_to_raw_downsampling(request, source_csv, expected_output_size):
+    """Test source_to_raw downsamples images + masks to the correct size."""
+
+    source_csv_path = request.getfixturevalue(source_csv)
+    output_dir = source_csv_path.parents[1]
+    output_vox_size = 20
+    source_to_raw(source_csv_path, output_dir, output_vox_size)
+
+    subject_dir = output_dir / "raw" / "sub-b"
+    image_path = subject_dir / "sub-b_res-20x20x20um_origin-asr.nii.gz"
+    mask_path = subject_dir / "sub-b_res-20x20x20um_mask_origin-asr.nii.gz"
+
+    output_vox_sizes_mm = (
+        output_vox_size * 0.001,
+        output_vox_size * 0.001,
+        output_vox_size * 0.001,
+    )
+
+    for output_path in [image_path, mask_path]:
+        image = load_nii(output_path, as_array=False)
+        assert image.header.get_zooms() == output_vox_sizes_mm
+        assert image.shape == expected_output_size
