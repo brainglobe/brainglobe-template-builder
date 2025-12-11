f5bff748efefe4d93538b1145ee7bfe34a1c1187
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-12-11 16:29:09 +0000

    initial test for PreprocWidgets

---
+++ tests/test_unit/napari/test_preproc_widgets.py
@@ -0,0 +1,34 @@
+from brainglobe_template_builder.napari._widget import PreprocWidgets
+
+
+def test_preproc_widgets(make_napari_viewer):
+    """
+    Test PreprocWidgets contains all relevant widgets with the correct titles.
+    """
+    viewer = make_napari_viewer()
+    preproc_widgets = PreprocWidgets(viewer)
+    viewer.window.add_dock_widget(preproc_widgets)
+
+    # Should be 4 widgets included in PreprocWidgets
+    n_widgets = 4
+    assert len(preproc_widgets.collapsible_widgets) == n_widgets
+    assert (
+        len(preproc_widgets.layout()) == n_widgets + 1
+    )  # layout has one extra spacer item
+
+    # Widgets should match the expected titles, and only the first should
+    # expanded (rest are collapsed by default)
+    expected_widget_titles = [
+        "Reorient to standard space",
+        "Create mask",
+        "Align midplane",
+        "Save files",
+    ]
+    for i, title in enumerate(expected_widget_titles):
+        widget = preproc_widgets.collapsible_widgets[i]
+        assert widget.text() == title
+
+        if i == 0:
+            assert widget.isExpanded()
+        else:
+            assert not widget.isExpanded()
