c2fa923333df4b9a064a3f80b02a18e991be1608
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-12-11 17:06:05 +0000

    add test for re-orientation widget

---
+++ tests/test_unit/napari/test_reorient_widget.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pytest
+from brainglobe_space import AnatomicalSpace
+
+from brainglobe_template_builder.napari.reorient_widget import Reorient
+
+
+@pytest.fixture
+def reorient_widget(make_napari_viewer):
+    """
+    Create a viewer, add the re-orient widget, and return the widget.
+    The viewer can be accessed using ``widget.viewer``.
+    """
+    viewer = make_napari_viewer()
+    widget = Reorient(viewer)
+    viewer.window.add_dock_widget(widget)
+    return widget
+
+
+@pytest.fixture()
+def stack() -> np.array:
+    """Create 50x50x50 stack with a small off-centre object (value 0.5)."""
+    stack = np.zeros((50, 50, 50))
+    stack[10:30, 10:30, 10:30] = 0.5
+    return stack
+
+
+def test_reorient_stack(reorient_widget, stack):
+    """Test the widget correctly re-orients ONLY the selected
+    image from SAL -> ASR"""
+    viewer = reorient_widget.viewer
+
+    # Add two images - we will re-orient one of them
+    viewer.add_image(stack, name="stack_to_reorient")
+    viewer.add_image(stack, name="stack_to_NOT_reorient")
+
+    # Select only one of the layers
+    viewer.layers.selection.clear()
+    viewer.layers.selection.add(viewer.layers["stack_to_reorient"])
+
+    # Starting orientation = SAL
+    reorient_widget.source_origin1.setCurrentText("s")
+    reorient_widget.source_origin2.setCurrentText("a")
+    reorient_widget.source_origin3.setCurrentText("l")
+
+    # End orientation = ASR
+    reorient_widget.target_origin1.setCurrentText("a")
+    reorient_widget.target_origin2.setCurrentText("s")
+    reorient_widget.target_origin3.setCurrentText("r")
+
+    reorient_widget.reorient_layers()
+
+    reoriented_image = viewer.layers["stack_to_reorient_orig-asr"].data
+    not_reoriented_image = viewer.layers["stack_to_NOT_reorient"].data
+    expected_reoriented_image = AnatomicalSpace("SAL").map_stack_to(
+        "ASR", stack
+    )
+
+    assert len(viewer.layers) == 2
+    np.testing.assert_array_equal(reoriented_image, expected_reoriented_image)
+    np.testing.assert_array_equal(not_reoriented_image, stack)
