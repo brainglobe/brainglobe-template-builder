ca6f3f0f66ff20fb5c4852de6a601e319fb91877
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2025-12-12 16:01:10 +0000

    add test for napari save widget

--- tests/test_unit/napari/test_reorient_widget.py
+++ tests/test_unit/napari/test_reorient_widget.py
@@ -29,26 +29,6 @@ def reorient_widget(make_napari_viewer):
     return widget
 
 
-@pytest.fixture()
-def stack() -> np.ndarray:
-    """Create 50x50x50 stack with a small off-centre object (value 0.5)."""
-    stack = np.zeros((50, 50, 50))
-    stack[10:30, 10:30, 10:30] = 0.5
-    return stack
-
-
-@pytest.fixture()
-def points() -> np.ndarray:
-    """Two example 3D points."""
-
-    return np.array(
-        [
-            [2, 3, 4],
-            [5, 6, 7],
-        ]
-    )
-
-
 def test_reorient_stack(reorient_widget, stack):
     """Test the widget correctly re-orients ONLY the selected
     image from SAL -> ASR"""
