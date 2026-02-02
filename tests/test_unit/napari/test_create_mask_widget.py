d70d814299c7f707a0e7637de739cc2a1eae1440
Stella <30465823+stellaprins@users.noreply.github.com>
2026-02-02 11:18:04 +0000

    Add test_create_mask_layer_data

--- tests/test_unit/napari/test_create_mask_widget.py
+++ tests/test_unit/napari/test_create_mask_widget.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pytest
 
 from brainglobe_template_builder.napari.mask_widget import CreateMask
+from brainglobe_template_builder.utils.masking import create_mask
 
 
 @pytest.fixture
@@ -19,3 +21,57 @@ def test_create_mask_creates_layer(mask_widget):
     initial_layer_count = len(mask_widget.viewer.layers)
     mask_widget._on_create_mask_button_click()
     assert len(mask_widget.viewer.layers) == initial_layer_count + 1
+
+
+@pytest.mark.parametrize(
+    "mask_config",
+    [
+        pytest.param(
+            {
+                "default": {
+                    "gauss_sigma": 3,
+                    "threshold_method": "triangle",
+                    "closing_size": 5,
+                    "erode_size": 0,
+                }
+            },
+            id="default",
+        ),
+        pytest.param(
+            {
+                "custom": {
+                    "gauss_sigma": 1,
+                    "threshold_method": "otsu",
+                    "closing_size": 3,
+                    "erode_size": 2,
+                }
+            },
+            id="custom otsu",
+        ),
+    ],
+)
+def test_create_mask_layer_data(mask_widget, stack, mask_config):
+    """Test that the created mask layer contains the correct data.
+
+    Applying different masking configurations (default and custom)
+    produce a mask layer with data matching the expected output.
+    """
+
+    if "default" in mask_config:
+        mask_config = mask_config["default"]
+
+    elif "custom" in mask_config:
+        mask_config = mask_config["custom"]
+        mask_widget.gauss_sigma.setValue(mask_config["gauss_sigma"])
+        mask_widget.threshold_method.setCurrentText(
+            mask_config["threshold_method"]
+        )
+        mask_widget.closing_size.setValue(mask_config["closing_size"])
+        mask_widget.erode_size.setValue(mask_config["erode_size"])
+
+    expected_mask_data = create_mask(stack, **mask_config)
+
+    mask_widget._on_create_mask_button_click()
+    mask_layer = mask_widget.viewer.layers[-1]
+
+    np.testing.assert_array_equal(mask_layer.data, expected_mask_data)
